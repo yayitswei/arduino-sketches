6a8a10ef4e2c4cc2821f1c9fcb61cff64919b704
Xuan <xuanwu@stanford.edu>
2012-11-09 23:20:05 -0800

    Decomposed the code and added a header file for song information. - Added a process_command function - Reduced main code line count to less than 100 - Reduced binary size to 6542

--- jukebox/jukebox.ino
+++ jukebox/jukebox.ino
@@ -1,6 +1,5 @@
-// Capital letter is sharp
-// Normal letter is normal
-// A = 440 is in the third octave
+// Capital letter is sharp, lowercase letter is normal, A = 440 Hz is in the third octave
+#include "songs.h"
 
 #define OCTAVE_MULTIPLIER 2
 #define HALF_STEP 1.059463094359295
@@ -9,19 +8,8 @@
 #define BASE_A 440
 #define TEMPO_PIN A1
 #define MODE_ROTATE -1
+#define MIDDLE_OCTAVE 3
 
-
-char* songs[] = {"O4c1c1c1c3  O3G3A3  O4c1_1O3A1O4c3_6  _6_6\0",
-                 "e3 d1 c2 d2 e2 e2 e4 d2 d2 d4 e2 g2 g4 e3 d1 c2 d2 e2 e2 e2 e2 d2 d2 e2 d2 c4 _4",
-                 "O3 g1 _1 g1 a3 g3 O4 c3 O3 b3 _3 | O3g1_1g1a3g3O4d3c3_3  O3g1_g1O4g3e3c3O3b3a3_3_3  O4f1_1f1e3c3d3c3_3_3_3_3",
-                 "O3e2g1f5O2b2O3d1c5  O4c1_1c1O3b5a1_1a1g5  O3e2g1f5O2b2O3d1c5  O4c1_1c1O3b2a1g1_1a1b2_1O4c1O3g1e1c1",
-                 "O3_1_1g1F1f1D2e2O2G1a1c2a1O3c1d1  _1_1g1F1f1D2e2c1_1c1c1_3  O3_1_1g1F1f1D2e2O2G1a1c2a1O3c1d  _1D3d3c3_2_4",
-                 "O3d5f1a1O4c1O3b1O4c1O3b3a1f1e1  O3d5f1a1O4c1O3b1O4c1O3b3a1b1O4c1  O4d5f1a1O4c1O3b1O4c1O3b3a1f1e1  O3d5f1a1O4c1e1e1d1g3_2",
-                 "O3g2O4e2d2c2O3g4_2g1g1  g2O4e2d2c2O3a4_2a1a1  a2O4f2e2d2O3b4  O4g2g2f2d2e4_4  O3g2O4e2d2c2O3g4_2g1g1  g2O4e2d2c2O3a4_2a1a1  a2O4f2e2d2g2g2g2g2  a2g2f2d2c4_4 _4_4",
-                 "O3e2e2e2_2  e2e2e2_2  e2g2c3d1e4_4  f2f2f2_1f1  f2e2e2_1e1  e2d2d2e2d4g4  O3e2e2e2_2  e2e2e2_2  e2g2c3d1e4_4  f2f2f2_1f1  f2e2e2e1e1  g2g2a2b2O4c2_2  O2c1_3",
-                 "O2c3d3D3e3  f1g1f4  _6  O2c3d3D3e3  f1g1f4  _6  O3c6  O2b6  O3d6c6  c3O2b3a3g3  e1d1c4  _6"};
-
-int g_num_songs = 9;
 int g_song_num = MODE_ROTATE;
 int g_song_num_changed = false;
 
@@ -40,8 +28,6 @@ void loop() {
 }
 
 void parse_song(char* song) {
-  int octave = 3;
-  
   // If the character is a number, skip, because the previous
   // letter should have taken care of the instruction
   for (unsigned int i=0; i<1000; i++) {
@@ -52,22 +38,25 @@ void parse_song(char* song) {
     
     param = param - '1' + 1;
     
-    // Read tempo
-    int base_duration = analogRead(TEMPO_PIN)/10 + 100; // Minimum will be 100 ms
-    
     if (g_song_num_changed) {
       g_song_num_changed = false;
       break;
     }
-      
-    int duration = base_duration;
-    if (command == 'O') {
-      octave = param;
-    } else if (command == '_') {
-      play_rest(base_duration * param);
-    } else if ((command >= 'a' && command <= 'g') || (command >= 'A' && command <= 'G')) {
-      play_note(command, octave, base_duration * param);
-    }
+
+    // Read tempo
+    int base_tempo_ms = analogRead(TEMPO_PIN)/10 + 100; // Minimum will be 100 ms    
+    process_command(command, param, base_tempo_ms * param);
+  }
+}
+
+void process_command(char command, int param, int tempo) {
+  static unsigned int octave = 3;
+  if (command == 'O') {
+    octave = param;
+  } else if (command == '_') {
+    play_rest(tempo);
+  } else if ((command >= 'a' && command <= 'g') || (command >= 'A' && command <= 'G')) {
+    play_note(command, octave, tempo);
   }
 }
 
@@ -76,20 +65,16 @@ void play_rest(int duration_ms) {
 }
 
 void play_note(char note, unsigned int octave, int duration_ms) {
-  boolean sharp = (note - 'a' < 0);
+  int sharp = (note - 'a' < 0) ? 1 : 0; // Check if letter is capital
+  if (sharp) note += 'a' - 'A'; // Convert note to lower case
+  
   int half_steps[7] = {0, 2, -9, -7, -5, -4, -2};
+  int hs = half_steps[note - 'a']; // number of half steps from 'a' to the correct note
   
-  // Convert note to lower case
-  if (sharp) note += 'a' - 'A';
-  int hs = half_steps[note - 'a'];
+  // Take into account the octave, and then shift frequency by the correct number of half steps
+  double freq = BASE_A * pow(OCTAVE_MULTIPLIER, octave - MIDDLE_OCTAVE) * pow(HALF_STEP, hs) * (HALF_STEP * sharp);
   
-  // Take into account the octave
-  double freq = BASE_A;
-  freq *= pow(OCTAVE_MULTIPLIER, octave)/8.0;
-  freq *= pow(HALF_STEP, hs);
-  if (sharp) freq *= HALF_STEP;
-  
-  digitalWrite(LED_PIN, HIGH);
+  digitalWrite(LED_PIN, HIGH); // Visual representation of a note being played
   tone(OUTPUT_PIN, int(freq), duration_ms);
   
   delay(duration_ms);
